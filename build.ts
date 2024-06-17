a225d86d13489f8880e4dfe68dea260a184d371c
YieldRay <yieldray@outlook.com>
2024-06-17 20:12:51 +0800

    update build script to use unbuild

--- build.ts
+++ build.ts
@@ -1,25 +1,25 @@
-import * as esbuild from 'npm:esbuild'
-
-console.log(
-    await esbuild.build({
-        entryPoints: ['src/index.ts'],
-        bundle: true,
-        outdir: 'dist',
-        format: 'esm',
-    }),
-)
+import { execSync } from 'node:child_process'
 
 const pkg = JSON.parse(Deno.readTextFileSync('./deno.json'))
 
 try {
     Deno.removeSync('./package.json')
+    console.log('Previous package.json removed.')
 } catch { // NOOP
 }
 
 const packageJson = {
     name: pkg.name,
     version: pkg.version,
-    main: 'dist/index.js',
+    type: 'module',
+    exports: {
+        '.': {
+            import: './dist/index.mjs',
+            require: './dist/index.cjs',
+        },
+    },
+    main: './dist/index.cjs',
+    types: './dist/index.d.ts',
     files: ['dist'],
     author: 'YieldRay',
     license: 'MIT',
@@ -33,6 +33,9 @@ const packageJson = {
     homepage: 'https://github.com/YieldRay/json-rpc-ts#readme',
 }
 
+Deno.writeTextFileSync('./package.json', JSON.stringify(packageJson, null, 4))
+console.log('New package.json created.')
 console.log(packageJson)
 
-Deno.writeTextFileSync('./package.json', JSON.stringify(packageJson, null, 4))
+console.log('Building package using unbuild...')
+execSync('npx unbuild@2.0.0', { stdio: ['ignore', 'inherit', 'inherit'] })
